1003567865237b9f668014add1cba4da5ee8534d
Jady Godoi <jady.godoi@oi.net.br>
2026-01-03 17:31:47 -0300

    feat: first commit - inicio do projeto

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyHomePage(),);
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Personal Expenses'),
+      ),
+      body: const Center(
+        child: Text('Welcome to Personal Expenses App!'),
+      ),
+    );
+  }
+}
