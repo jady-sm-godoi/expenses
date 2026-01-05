bfdc2cfb842055a22491312c9d51da6fda1630bb
Jady Godoi <jady.godoi@oi.net.br>
2026-01-05 09:10:39 -0300

    feat: model para transações

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(ExpensesApp());
@@ -13,7 +14,41 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  MyHomePage({super.key});
+
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Café',
+      value: 35.90,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Pão',
+      value: 12.50,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't3',
+      title: 'Leite',
+      value: 8.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't4',
+      title: 'Açúcar',
+      value: 6.40,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't5',
+      title: 'Manteiga',
+      value: 15.30,
+      date: DateTime.now(),
+    ),
+  ];
+
 
   @override
   Widget build(BuildContext context) {
@@ -21,9 +56,20 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Personal Expenses'),
       ),
-      body: const Center(
-        child: Text('Welcome to Personal Expenses App!'),
-      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Card(
+            elevation: 5,
+            child: Text('Grafico'),
+          ),
+          Card(
+            elevation: 5,
+            child: Text('Lista de Transações'),
+          ),
+        ],
+      )
     );
   }
 }
